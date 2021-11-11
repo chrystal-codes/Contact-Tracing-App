9a11005bc7ae722dd6b1201da909965751962653
chrystal-codes <78388074+chrystal-codes@users.noreply.github.com>
2021-11-10 21:28:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
+var intervalID;
 var running = false;
-	console.log(running);
-	window.setInterval(sendData,120000);  
+console.log(running);
 
-	function sendData(){
-		console.log("attempting to send data...");
-		if (running == true){
-			axios.post('https://sheetdb.io/api/v1/a2p84tz3nd0mb?sheet='+roomNum,
-					  {"data":{"studentID":studentID}
-			  }).then(response => {
-			console.log("data sent!");
-				console.log(response);
-			})
-		}
-		else{
-			console.log("no data to send. failure");
-		}
+document.getElemntById("submit-form").addEventListener("click",submitForm);
+
+function createStopBtn(){
+	var stopBtn = document.createElement("button");
+	stopBtn.innerText = "STOP";
+	document.getElementById("sheetdb-form").appendChild(stopBtn);
+	stopBtn.addEventListener("click",clearInterval(intervalId));
+}
+
+function sendData(){
+	console.log("attempting to send data...");
+	if (running == true){
+		axios.post('https://sheetdb.io/api/v1/a2p84tz3nd0mb?sheet='+roomNum,
+				  {"data":{"studentID":studentID}
+		  }).then(response => {
+		console.log("data sent!");
+			console.log(response);
+		})
 	}
-	function submitForm(){
-		var studentID = document.getElementById("student_id").value;
-		var roomNum = document.getElementById("room_number").value;
+	else{
+		console.log("no data to send. failure");
+	}
+}
+function submitForm(){
+	var studentID = document.getElementById("student_id").value;
+	var roomNum = document.getElementById("room_number").value;
 
-		if (studentID != null && roomNum != null){
-			running = true;
-		}
-
+	if (studentID != null && roomNum != null){
+		running = true;
 		console.log("form submitted!");
+		sendData();
+		createStopBtn();
+		intervalId = window.setInterval(sendData,60000); 
 	}
+	else {
+		console.log("data could not be sent");
+	}
+
+	 
+}
