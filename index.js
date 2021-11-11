110c75892ab7020879bbc27d681fe73f2bda36bb
chrystal-codes <78388074+chrystal-codes@users.noreply.github.com>
2021-11-11 12:59:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,43 @@
-var intervalID;
-var running = false;
-console.log(running);
-document.body.innerHTML += running;
+ var studentID;
+  var roomNum;
+  var loc;
+  var running = false;
 
-document.getElemntById("submit-form").addEventListener("click",submitForm);
-
-/*function createStopBtn(){
-	var stopBtn = document.createElement("button");
-	stopBtn.innerText = "STOP";
-	document.getElementById("sheetdb-form").appendChild(stopBtn);
-	stopBtn.addEventListener("click",clearInterval(intervalId));
-}*/
-
-function sendData(room,id){
-	console.log("attempting to send data...");
-	if (running == true){
-		axios.post('https://sheetdb.io/api/v1/a2p84tz3nd0mb',
-				  {"data":{"studentID":id}
-		  }).then(response => {
-			console.log("data sent!{"+id+","+room+"}");
-			document.body.innerHTML += "data sent!{"+id+","+room+"}";
-			console.log(response);
-		})
-	}
-	else{
-		console.log("no data to send. failure");
-		document.body.innerHTML += "no data to send. failure";
-	}
-}
-function submitForm(){
-	document.body.innerHTML += "clicked to submit form";
+  function success(){
+	  long = position.coords.longitude;
+	  lat = position.coords.latitude;
+	  
+	  return [long,lat]
+  }
+  function error(){
+	  console.log("Geolocation not supported in this browser");
+  } 
 	
-	var studentID = document.getElementById("student_id").value;
-	var roomNum = document.getElementById("room_number").value;
-
-	if (studentID != null && roomNum != null){
-		running = true;
-		console.log("form submitted!");
-		document.body.innerHTML += "form submitted!";
-		sendData(roomNum,studentID);
-		//createStopBtn();
-		window.setInterval(sendData,60000,roomNum,studentID); 
+  function sendData(id,room){
+	if(id != null && room != null){
+		axios.post('https://sheetdb.io/api/v1/a2p84tz3nd0mb?sheet=sheet1',
+				  {"data":{"studentID":id,"room":room/*,"longitude":loc[0],"latitude":loc[1]*/}
+		  }).then(response => {
+			console.log("data sent!");
+			console.log(response);
+		  })
 	}
-	else {
-		console.log("data could not be sent");
-		document.body.innerHTML += "data could not be sent";
-	}
-
-	 
-}
+  }
+  function submitForm(){
+  	running = true;
+    
+    studentID = document.getElementById("student_id").value;
+    roomNum = document.getElementById("room_number").value;
+	loc = navigator.geolocation.getCurrentPosition(success,error);
+    
+	sendData(studentID,roomNum);
+    console.log("success!");
+  }
+  function clickBtn(){
+	if(studentID.length == 6 && roomNum >= 1000 && roomNum <= 3100){
+		document.getElementById("submit-form").click();	
+	}	
+  }
+  
+  document.getElementById("submit-form").addEventListener("click",submitForm);
+  setInterval(clickBtn,120000);
